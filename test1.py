17d2c7e0b921ed8c630501dce7b9916a19083a61
spvyerra <spvyerra@gmail.com>
2019-03-10 10:32:30 -0400

    Basic set up for transactions

--- test1.py
+++ test1.py
@@ -1,5 +1,39 @@
-from blockChain import BlockChain
+##Ensures that the Block class functions properly
 
-localBlockchain = BlockChain()
+from block import Block
 
-localBlockchain.printBlocks()
+transaction1 = {
+    'amount' : '30',
+    'sender' : 'Bob',
+    'reciever' : 'Sarah'
+}
+
+transaction2 = {
+    'amount' : '45',
+    'sender' : 'Steve',
+    'reciever' : 'Jobs'
+}
+
+transaction3 = {
+    'amount' : '85',
+    'sender' : 'Bill',
+    'reciever' : 'Gates'
+}
+
+transaction4 = {
+    'amount' : '25',
+    'sender' : 'Alice',
+    'reciever' : 'Timmy'
+}
+
+mempool = [transaction1,transaction2,transaction3,transaction4]
+
+# My new Transaction
+myTransaction = {
+    'amount' : '55',
+    'sender' : 'George',
+    'reciever' : 'Bob'
+}
+
+mempool.append(myTransaction)
+
